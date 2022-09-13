3f7e23e8cbfe0a9a879dce8aaeeb46950a985088
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-09-14 00:56:58 +0400

    minor improvment to dockstring and variable naming

--- merge.py
+++ merge.py
@@ -7,7 +7,7 @@ def merge(left: Union[list, tuple], right: Union[list, tuple]) -> Union[list, tu
     collections combined in sorted order
     :param left: first sorted collection
     :param right: second sorted collection
-    :return: a sorted collection
+    :return: a resulting sorted collection
     """
     result = []
     if len(left) == 0:
@@ -15,27 +15,27 @@ def merge(left: Union[list, tuple], right: Union[list, tuple]) -> Union[list, tu
     elif len(right) == 0:
         result = [item for item in left]
     else:
-        left_gen, right_gen = iter(left), iter(right)
-        left_item, right_item = next(left_gen), next(right_gen)
+        left_iterator, right_iterator = iter(left), iter(right)
+        left_item, right_item = next(left_iterator), next(right_iterator)
 
         while True:
             if left_item <= right_item:
                 result.append(left_item)
                 try:
-                    left_item = next(left_gen)
+                    left_item = next(left_iterator)
                 except StopIteration:
                     result.append(right_item)
                     break
             else:
                 result.append(right_item)
                 try:
-                    right_item = next(right_gen)
+                    right_item = next(right_iterator)
                 except StopIteration:
                     result.append(left_item)
                     break
-        for item in left_gen:
+        for item in left_iterator:
             result.append(item)
-        for item in right_gen:
+        for item in right_iterator:
             result.append(item)
 
     return tuple(result) if isinstance(left, tuple) and isinstance(right, tuple) else result
@@ -49,4 +49,3 @@ if __name__ == '__main__':
     assert merge((1, 2), (3, 5, 7)) == (1, 2, 3, 5, 7)
     assert merge((), (3, 5, 7)) == (3, 5, 7)
     assert merge([1, 2, 5], []) == [1, 2, 5]
-    
