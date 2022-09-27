9824c0da04653e2c053ecc7940d6dee22bfedde0
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-09-27 19:03:19 +0400

    changed pythagoras

--- assignment_3/pythagoras.py
+++ assignment_3/pythagoras.py
@@ -1,5 +1,7 @@
 import math
 
+from itertools import combinations
+
 from typing import Generator
 
 
@@ -10,12 +12,15 @@ def get_pythagoras_triples(number: int) -> Generator:
     :param number: a given number
     :return: a generator of pythagoras triples
     """
-    return ((a, b, int(math.sqrt(a ** 2 + b ** 2)))
-            for b in range(number)
-            for a in range(1, b)
-            if math.sqrt(a ** 2 + b ** 2) % 1 == 0)
+    return (
+        (a, b, int(math.sqrt(a ** 2 + b ** 2)))
+        for a, b in combinations(range(1, number + 1), 2)
+        if math.sqrt(a ** 2 + b ** 2) % 1 == 0
+           and 1 <= int(math.sqrt(a ** 2 + b ** 2)) <= number
+    )
 
 
 if __name__ == '__main__':
+    print(list(get_pythagoras_triples(12)))
     assert list(get_pythagoras_triples(12)) == [(3, 4, 5), (6, 8, 10)]
 
