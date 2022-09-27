11be5c6a0e57da7487b431e19f809790297c8155
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-09-27 22:02:21 +0400

    three variants of flatten done

--- assignment_3/functional.py
+++ assignment_3/functional.py
@@ -23,8 +23,54 @@ def flatten(sequence: list[Any]) -> Generator:
                     queue.extendleft(item)
 
 
+def flatten2(sequence: list[Any]) -> Generator:
+    """
+    The function flattens nested iterables (except for strings) without
+    using recursion
+    :param sequence: a sequence with nested iterables
+    :return: generator of sequence contents
+    """
+    stack = deque()
+    for item in sequence:
+        if not isinstance(item, Iterable) or isinstance(item, str):
+            yield item
+        else:
+            stack.append(item)
+            while stack:
+                popped = iter(stack.pop())
+                for sub in popped:
+                    if not isinstance(sub, Iterable) or isinstance(sub, str):
+                        yield sub
+                    else:
+                        stack.append(popped)
+                        stack.append(sub)
+                        break
+
+
+def flatten3(sequence: list[Any]) -> Generator:
+    """
+    The function flattens nested iterables (except for strings) without
+    using recursion
+    :param sequence: a sequence with nested iterables
+    :return: generator of sequence contents
+    """
+    stack = deque()
+    for item in sequence:
+        stack.append(item)
+        while stack:
+            popped = stack.pop()
+            if not isinstance(popped, Iterable) or isinstance(popped, str):
+                yield popped
+            else:
+                popped = iter(popped)
+                for sub in popped:
+                    stack.append(popped)
+                    stack.append(sub)
+                    break
+
+
 if __name__ == '__main__':
     expected = [1, 2, 0, 1, 1, 2, 1, 'ab']
-    actual = flatten([1, 2, range(2), [[], [1], [[2]]], (x for x in [1]), 'ab']
+    actual = flatten3([1, 2, range(2), [[], [1], [[2]]], (x for x in [1]), 'ab']
                      )
     assert expected == list(actual)
