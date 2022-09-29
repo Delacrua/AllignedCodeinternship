583d18ba3b558ee05421efe1b2a57265859f1155
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-09-29 16:07:07 +0400

    refactored client code in OOP

--- TCPchat/tcp_client.py
+++ TCPchat/tcp_client.py
@@ -1,40 +1,49 @@
 import socket
 import threading
 
-NICK = input('Choose a nickname: ')
-
 HOST = '127.0.0.1'
 PORT = 52525
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+class TCPChatClient:
+
+    def __init__(self, host=HOST, port=PORT, sock=None):
+        self.host = host
+        self.port = port
+        self.nickname = input('Choose a nickname: ')
+        if sock is None:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send_message(self):
+        while True:
+            message = f'{self.nickname}: {input("")}'
+            self.socket.send(message.encode('utf-8'))
+
+    def receive(self):
+        while True:
+            try:
+                message = self.socket.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.socket.send(self.nickname.encode('utf-8'))
+                else:
+                    print(message)
+            except Exception as exc:
+                print(f'An error occurred: {exc}')
+
+    def connect_to_server(self):
+        self.socket.connect((self.host, self.port))
+        receive_thread = threading.Thread(target=self.receive)
+        write_thread = threading.Thread(target=self.send_message)
+        receive_thread.start()
+        write_thread.start()
 
 
-def receive():
-    while True:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            if message == 'NICK':
-                client.send(NICK.encode('utf-8'))
-            else:
-                print(message)
-        except Exception as exc:
-            print(f'An error occurred: {exc}')
-
-
-def write():
-    while True:
-        message = f'{NICK}: {input("")}'
-        client.send(message.encode('utf-8'))
-
-
-receive_thread = threading.Thread(target=receive)
-write_thread = threading.Thread(target=write)
+def main():
+    client = TCPChatClient()
+    client.connect_to_server()
 
 
 if __name__ == '__main__':
-    receive_thread.start()
-    write_thread.start()
+    main()
 
 
