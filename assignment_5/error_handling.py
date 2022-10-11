f4552670c814f08b934c81e1314fb1eaebd0f2a6
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-10-11 13:54:58 +0400

    refactored  task 1

--- assignment_5/error_handling.py
+++ assignment_5/error_handling.py
@@ -4,8 +4,7 @@ import sys
 import traceback
 
 from time import sleep
-from typing import Optional, Union, Type
-
+from typing import Optional, Union, Type, Callable
 
 logger = logging
 log_format = "%(levelname)s %(asctime)s - %(message)s"
@@ -24,9 +23,34 @@ def handle_error(
         tries: Optional[int] = 1,
         delay: Union[int, float] = 0,
         backoff: int = 1,
-):
-    def decorator(func):
+) -> Callable:
+    """
+
+    :param re_raise: shows if Exceptions listed in exc_type will be
+    reraised
+    :param log_traceback: shows if traceback of Exceptions listed in
+    exc_type will be logged
+    :param exc_type: an Exception type or a tuple of Exception types
+    :param tries: a number of tries to call decorated function before
+    raising Exception
+    :param delay: delay between tries in seconds
+    :param backoff: a multiplier implied to delay after each try
+    :return: decorated function object
+    """
+    def decorator(func: Callable) -> Callable:
+        """
+        A decorator wraps given func with a wrapper
+        :param func: given func
+        :return: wrapped func
+        """
         def wrapper(*args, **kwargs):
+            """
+            a wrapper that implements the logics of handle_error
+            decorator, according to arguments given
+            :param args: any number of positional arguments
+            :param kwargs: any number of keyword arguments
+            :return:
+            """
             result = None
             func_delay = delay
 
@@ -43,23 +67,22 @@ def handle_error(
                 if tries <= 0:
                     raise ValueError('tries argument must be a positive integer')
                 else:
-                    for _ in range(tries - 1):
+                    func_tries = tries
+                    while func_tries:
+                        func_tries -= 1
                         try:
                             result = func(*args, **kwargs)
-                        except exc_type:
-                            sleep(func_delay)
-                            func_delay *= backoff
+                        except exc_type as exc:
+                            if func_tries:
+                                sleep(func_delay)
+                                func_delay *= backoff
+                            else:
+                                if re_raise:
+                                    raise exc
+                                if log_traceback:
+                                    logger.error(traceback.format_exc())
                         else:
                             break
-            if not result:
-                try:
-                    result = func(*args, **kwargs)
-                except exc_type as exc:
-                    if re_raise:
-                        raise exc
-                    if log_traceback:
-                        logger.error(traceback.format_exc())
-
             return result
 
         return wrapper
@@ -67,30 +90,40 @@ def handle_error(
 
 
 if __name__ == '__main__':
+    print('----------->Decorator block<-------------')
+    # # Example 1
     # @handle_error(re_raise=False)
     # def some_function():
     #     x = 1 / 0  # ZeroDivisionError
-    #
     # some_function()
     # print(1)  # line will be executed as exception is suppressed
 
-
+    # # Example 2
     # @handle_error(re_raise=False, exc_type=KeyError)
     # def some_function():
     #     x = 1 / 0  # ZeroDivisionError
     # some_function()
     # print(1)  # line won ’t be executed as exception is re - raised
 
+    # # Example 3
     # @handle_error(re_raise=False, log_traceback=False)
     # def some_function():
     #     x = 1 / 0  # ZeroDivisionError
     # some_function()
     # print(1)  # line will be executed as exception is suppressed
 
+    # # Example 4
+    # @handle_error(re_raise=True, tries=3, delay=0.5, backoff=2)
+    # def some_function():
+    #     if random.random() < 0.75:
+    #         x = 1 / 0  # ZeroDivisionError
+    # some_function()
 
-    @handle_error(re_raise=True, tries=3, delay=0.5, backoff=2)
-    def some_function():
-        if random.random() < 0.75:
-            x = 1 / 0  # ZeroDivisionError
+    # # Example 5 (testing tries=None)
+    # @handle_error(re_raise=True, tries=None, delay=0.5, backoff=2)
+    # def some_function():
+    #     if random.random() < 0.75:
+    #         x = 1 / 0  # ZeroDivisionError
+    # some_function()
 
-    some_function()
+    print('----------->Context manager block<-------------')
