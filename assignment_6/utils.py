755d82f8e56bea2a6848139e84e3cedf0fa6430b
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-10-18 15:18:23 +0400

    Implemented first part of task 1

---
+++ assignment_6/utils.py
@@ -0,0 +1,47 @@
+import datetime
+import requests
+
+from requests.exceptions import ConnectTimeout
+from typing import Union
+
+_NOT_SET = object()
+
+
+class SafeRequest:
+
+    def __init__(self,
+                 timeout: Union[datetime.timedelta, float] = 3.0,
+                 default=_NOT_SET,
+                 ):
+        self._timeout = timeout
+        self.default = default
+
+    def __call__(self, url: str, *args, **kwargs):
+        with requests.Session() as session:
+            try:
+                response = session.get(url=url, timeout=self._timeout)
+            except ConnectTimeout as exc:
+                raise exc
+        if response.status_code == requests.codes.ok:
+            return response
+        elif response.status_code == requests.codes.not_found:
+            return self.default
+        else:
+            response.raise_for_status()
+
+    @property
+    def timeout(self):
+        return self._timeout
+
+    @timeout.setter
+    def timeout(self, new_value):
+        if isinstance(new_value, datetime.timedelta):
+            self._timeout = new_value.total_seconds()
+        else:
+            self._timeout = new_value
+
+
+if __name__ == '__main__':
+    test_url1 = 'https://api.github.com/events'
+    getter = SafeRequest(timeout=0.02)
+    resp = getter(url=test_url1)
